70de2662e074d5545e5d396f4f96edfaae2d8ab0
agasurfer <agasurfer@hotmail.com>
2024-09-10 16:08:59 +0200

    Create exercise on function.js
    Exercises on Functions

---
+++ exercise on function.js
@@ -0,0 +1,90 @@
+/*
+let tries = 1
+
+
+
+function game(tries, face ) {
+
+    console.log(`Die ${tries} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    console.log(`Die ${tries + 1} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    console.log(`Die ${tries + 2} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    console.log(`Die ${tries + 3} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    console.log(`Die ${tries + 4} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    console.log(`Die ${tries + 5} : ${Math.floor(Math.random() * 6 + 1 )}`);
+    }
+
+game(tries, face)
+
+
+const myVariable = sum(3, 6) 
+ 
+function sum(a , b) { 
+    return a + b
+};
+
+console.log(myVariable);
+
+
+
+
+function isShortWeather(temperature) {
+    if (temperature >= 24) {
+        return console.log("true");
+    } else {
+        return console.log("false");
+    }
+}
+isShortWeather(11);
+
+
+
+function Myfunction(a, b){
+  return console.log(a.repeat(b))
+     ;
+    
+}
+Myfunction("dir", 5);
+
+
+
+
+
+function greet(firstName, lastName){
+    console.log(`This is ${firstName} ${lastName.charAt(0)}.`)
+}
+greet("Linus", "Torvalds");
+
+
+function lastElement(array){
+    if (array.length !== 0){ 
+  return array[array.length - 1]
+ }  else {
+    return null;
+ }
+}
+
+console.log(lastElement([]))
+
+
+
+function capitalize(word){
+   upper = word.toUpperCase()
+   initial = upper.substring(0 , 1)
+   rest = word.slice(1) 
+    
+ console.log(`${initial}${rest}`)
+}
+
+capitalize("congolexicomatisation")
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+function returnDay(day) {
+if (day < days.length + 1 ) {
+    console.log(days[day - 1] )
+} else {
+    console.log("null")
+}
+}
+
+returnDay(7)
+*/
