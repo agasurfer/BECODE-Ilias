a790ff22fdebf21a23e7b1b51c439414f510e740
agasurfer <agasurfer@hotmail.com>
2024-09-12 13:53:37 +0200

    Perdu du temps avant de me rendre compte qu'il suffisait de mettre "array" comme paramêtre

---
+++ Week-03-04/01ALGOS-serie103exo.js
@@ -0,0 +1,16 @@
+let testArray = [8, 15, 2, 97, 6485,2, 8]
+
+
+function minMax(array) {
+
+  const min = Math.min(...array);
+  const max = Math.max(...array) 
+  console.log([min, max]);
+  }
+    
+minMax([1, 2, 3, 4, 5]);
+minMax([2334454, 5]);
+minMax([1, 1]);
+
+
+
