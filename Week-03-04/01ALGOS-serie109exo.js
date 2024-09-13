130aa793865bcec48208f45511d2cf3af4a68898
agasurfer <agasurfer@hotmail.com>
2024-09-13 11:19:24 +0200

    Simple calcul

---
+++ Week-03-04/01ALGOS-serie109exo.js
@@ -0,0 +1,14 @@
+function calculateFuel(dist) {
+let fuel = dist * 10
+if (fuel > 100) {
+    console.log(fuel);
+    
+} else {
+    console.log("100");
+    
+}
+}
+
+calculateFuel(15);
+calculateFuel(23.5);
+calculateFuel(3);
