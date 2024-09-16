f35409fda2f5b0c67f63ccd55afbfa5b5520299c
agasurfer <agasurfer@hotmail.com>
2024-09-16 09:25:02 +0200

    Juste à se rememorer parseFloat

---
+++ Week-03-04/02ALGOS-serie205exo.js
@@ -0,0 +1,14 @@
+
+function removeLeadingTrailing(n) {
+   
+    num = parseFloat(n);
+    mot = num.toString();
+    console.log(mot);
+    
+}
+
+removeLeadingTrailing("230.000")
+removeLeadingTrailing("00402")
+removeLeadingTrailing("03.1400")
+removeLeadingTrailing("30")
+
