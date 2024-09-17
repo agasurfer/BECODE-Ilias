5536a5a7c80dd97137711262700d88fd0194c307
agasurfer <agasurfer@hotmail.com>
2024-09-17 16:01:14 +0200

    Difficulté principale de bien garder le fil, le scope et l'histoire de modulo

---
+++ Week-03-04/04ALGOS-serie403exo.js
@@ -0,0 +1,37 @@
+function caesarCipher(str, num) {
+   
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+   
+    let final = ""
+   
+    num = num % 26  //Trouvé sur Stackflow
+    
+    for (let i = 0; i < str.length; i++) {
+    
+        let lettreActuelle = str[i].toLowerCase();
+        if (alphabet.includes(lettreActuelle)) {
+            
+            let indexActuel = alphabet.indexOf(lettreActuelle)
+            
+            let indexNew = indexActuel + num
+            
+            if (indexNew >= 26) indexNew -= 26  //Trouvé sur StackFlow
+            if (indexNew < 0) indexNew += 26    // Trouvé sur StackFlow
+            
+            let nouvLettre = alphabet[indexNew]
+
+            final += nouvLettre
+        } else {
+            final += lettreActuelle
+        }
+  }
+
+
+console.log(final);
+
+
+}
+
+caesarCipher("zoo keeper", 2); //----> "bqq mggrgt"
+caesarCipher("bqq mggrgt", -2); //----> "zoo keeper"
+caesarCipher("My name is Henrique", 3); //----> "pb qdph lv khqultxh "
