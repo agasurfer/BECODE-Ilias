9a6a9fd7f1cde6781a3c69d1a6dbe60c77929b5b
agasurfer <agasurfer@hotmail.com>
2024-09-24 09:20:24 +0200

    split à la lettre

---
+++ Week-03-04/05ALGOS-Optional08.js
@@ -0,0 +1,17 @@
+
+let occurencies = (str, letter)=> console.log(str.split(letter).length -1)
+
+
+occurencies("hello", "l");
+// Result : 2
+
+occurencies("abracadabra", "a");
+// Result : 5
+
+occurencies("oups", "z");
+// Result : 0
+
+
+
+
+
