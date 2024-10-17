b4fca04f7e16d2e194801fb5d716631d25429b7e
agasurfer <agasurfer@hotmail.com>
2024-10-17 10:04:23 +0200

    Added another way

--- Week 07/EventListenersExercises/Exercise 1/app.js
+++ Week 07/EventListenersExercises/Exercise 1/app.js
@@ -1,20 +1,29 @@
-const btnHello = document.querySelector("button");
+/* const btnHello = document.querySelector("button");
 
-const btnGdbye = btnHello.nextElementSibling
-/* btn.addEventListener("click", () => {
-    if (btn.innerText === "Hello") {
+const btnGdbye = btnHello.nextElementSibling;
+
+btnHello.addEventListener("click", () => {
+    alert("Oh my God! You again!!!")
+});
+
+btnGdbye.addEventListener("click", () => {
+    alert("Thank God ! I thought you'd' never leave !")
+}); */
+
+    const btns = document.querySelectorAll("button")
+
+    btns.forEach((btn) => { 
+
+    btn.addEventListener("click", () => {
+    
+        if (btn.innerText === "Hello") {
         alert("Oh my God! You again!!!")
+    
     } else if (btn.innerText === "Goodbye") {
         alert("Thank God ! I thought you'd' never leave !")
     }
         
     
-}) */
+}); 
 
-    btnHello.addEventListener("click", () => {
-        alert("Oh my God! You again!!!")
-    });
-
-    btnGdbye.addEventListener("click", () => {
-        alert("Thank God ! I thought you'd' never leave !")
-    });
+});   
