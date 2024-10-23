0a1413b486240beaf47c742df24917452d2f0b9b
agasurfer <agasurfer@hotmail.com>
2024-10-23 10:27:22 +0200

    comments added

--- Week 08/SlideMenu/app.js
+++ Week 08/SlideMenu/app.js
@@ -1,11 +1,15 @@
+
+// VARIABLES
 const hamburger = document.querySelector("p");
 const navbar = document.querySelector(".navbar")
 const cross = document.querySelector(".closebtn")
 
+// MAKES HAMBURGER OPEN MENU
 hamburger.addEventListener("click", () => {
     navbar.style.width = "250px"
 })
 
+// MAKES THE CROSS CLOSE THE MENU
 cross.addEventListener('click',() => {
     navbar.style.width = "0";
 })
