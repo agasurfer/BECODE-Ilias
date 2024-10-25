2c93ce49e7deac8b22a254115c93918902da01b9
agasurfer <agasurfer@hotmail.com>
2024-10-25 16:03:52 +0200

    Swiper exercise

---
+++ Week 08/ExerciseSwiper/app.js
@@ -0,0 +1,32 @@
+var menu = ["https://s3-alpha-sig.figma.com/img/c3bc/d288/953ca91f376c7885f3fde2a4af798b92?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DRU-gvdNCJyy7l8f91gQii5FRLhBrbOD-2Br15LL0yXtSQVcDKxPhxW0kAtvXqnjXaZZV51eD8ia2CpwJe3HMnmQBae0qsqAClBurmAxPNfJunEU88m5tkUYsG9h4ns8Q8Hu6NzVbnD55C2kuJSWtvOKwlBRxSDtgXv1WQbLZIcLMjFl0LYY6CAOad~Rx3K7i395HNmo57Sh9deX7V1bwaIFARPXN-f8~Dj2zlPgBvmT-5NDHxtEa0DecJWqoosfFa6wCXaWz-yfMh7E0rTFY9gOLUWG3vw3KKGM1n1CV6-Miq7EAF4sxFqIqM9kvnjPrXrRZEmuPTzxAaDyjJyCBg__", 
+    "https://s3-alpha-sig.figma.com/img/d030/e9d3/0936aef3b4a83e13318fa114da030d05?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bNFGR-y0foZBZ~TIGWDdVo4y688CSYnYUjKQyzG-iGnYLdSgCT4QpdjFqhgBDbhdqZTb5dSz3q03rrRr0QVrbnH0mVOZKLnhd4zAXgnIw25olLPaL40cqOYZEsqJOKkdKu3AJkEszoY9LczFI82AvVcFGSpDv9deHIzSqgc5r-M8w7BMsblkLvsEoHpa7EB4l81nMDao46uACL53vlE98vwa6GeUNZs4UZtxPELweV3mOyTcs4hv88UdqJ4Q4hism1AxCrP8Na8UmLBdXQwdvD0~Pfmxp3We8hDetJ3vBbPz4wi9OA4DGYENYN4OWA8e-yFw3VyPKw0MDhz0ngf5yQ__", 
+    "https://s3-alpha-sig.figma.com/img/bfa1/64e6/c9e4d83b17a7269dee7073f1ce540a0d?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q91Qi16cD52nje9r09LFHsWTAqbgRstgUB6pa6h4CV7c~AkFCRITZF6XoEE-6Dv8g8AL7hd5BLHgDzfUW~uVdHzvLH1fyeXE8phhpHu5B80qKYxaNZxMZzTiiXjKhnm2s~iIEAypDtARoqpnUAa89SSqRmNfHMCPtvbKsoV-Dd2va4Yiganl47xjhKa2esACORss34QIGF3kEXOBTYrTXn23pfO3odjVOSpbheeH6V80V9D4LbI4mVZtJItuNHsHfmj4rLlAoPklWby4P26BM05NmA4GBpMTe0aLh3AJM5gpUQI9J40FBJEk9hfC0KvPLfInOKvRS95MsUxBntmPvQ__",
+    "https://s3-alpha-sig.figma.com/img/3dbc/1890/70393e0955890490fb63abd6cbc74720?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JEJeVCrsA2-oW4~jinSqaq7Oj-M1cvbOkULFC742Wi-tv7PKcQB3xok133-7~S~KFn94bH5GkQjAUma~i30v4cwB8Su1rph0Cn4hFAxBmhi3LC~P~ZlVh-4mkIdQFWVZm5hKxQQRRrGeha5Y7PtMB-aKNNjUd5jCNW7hCAiUbD3M6a0Z99T60S7kouKCGCUscWyomB1tbZa9cRU9Azy-ovEt8at6US-ilmSuznuwWmn1a~vqKUdUsz~fUS85b2H2yHSHw-OyrP1TVti4CFqUWHfhyHQBVqs0EAx0p2Rxu1DOQy2LGL-MZZ7kywEjfSkAjJaCRJUVRiqo37lEvOsjOw__",
+    "https://s3-alpha-sig.figma.com/img/0a78/1456/d67b6be7fb932735cd4c88c669826689?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ql5MWb2scJ-NG781PwhkXA~BVKlMKyX0D129rWDn7src~Yijz5dcGCFwr0N~nvaS7L-Xg29W6odp5EHg8wRe0OYTIBwHy8n7BrmilbGGhxlDzyBuKT~-Yx6X2kRDJbeRwhiy-ccNehpyACbWQHhKDlVZzXd0NkcCJs24C-6p~hR6w7ZvMzd9Kv21ZQ-NhvxDWOdQltNERM6Jgo3v~LrV0J4MT5XTPzrEsYzKQrvjjoe4ergLgRpBUn5ZABUoiU-zG0VOUiDzPSeyJgTRM05u~UQx3nKRKDrziadKNtog~IqLF8fEneDRjWscUZYL3J2HYGEoDDBoa9QK-t32MvU2RA__",
+    "https://s3-alpha-sig.figma.com/img/d78b/af51/0079924e47642dc9625cde951f6d73eb?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c7HgdQdiV0cTg~sqFDBZNc1q8yAyC1rUbKAA51M4hTT~CQbZH0CzsvjHQAF9mULKwZrye0xlcjUEnjq~8a2VLHFql2PiGuzS~3AgPAay18gVA0WpHfE-usszdCiW2X0mHl5paGDANc76Qt85p0sDfmi~mAhrm30jSpk3~HbqEOeNarkI2Kfege6VKWpJcjBZQiW2nRiXs68VSTm9f412vI5xGD8X7RnVGcdHZ8WG2E3HHrtVkif47VJFmVDMW0GzWOSOqGO6BUXTlEuTkoyv47sbvKX5E0fZsh40WnzYj9g~HCQbPvHix0d-VThp3DKguuzvyxDXTV4NMdWsOUuM4w__",
+    "https://s3-alpha-sig.figma.com/img/2b35/e6da/6c17a9f7fc574cffc176420f61e22045?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Nh2dWcTB~3TYlcmga8x~gxN7E6UWYshhxC7kOG2d2un4sW3N7-83UO60lPqdz8cSZwcDw4KyXa~60x3NWCo04nwiF7PA3oKKk30E8K2xCTdWdlUnOdRNPP~p6RRGOdOSRwroyB-JCCnGWs-kVe5M-EZvjrQgIgrm32ki4-xm2LhbCVdOYZd4mW3W7o~nk82DunOno1lKqnXHQYcnSc2~CUD-x41ZrrorNnMohX3m4IG7JHnHpwMzQREx9j30lzU~-5Og6rFBzN7DehE4NMmD2ldUED7mpd7W~kn1mZQz5cpJYHbYgYe0k~9gdBhAa0Ea~x6VqIqin~B8ucRkgzMjLg__",
+    "https://s3-alpha-sig.figma.com/img/617b/a461/818a280d3397969471500fc85534eebe?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Pl7UFn8dL6OzufybusbfphlxAVsMzMXkXcogQn1YX8QFogN9LSrPCuoX8g~3AaLeJnr4Gh7g~zfbk7MfEIdkWvJSqjP6BTkBl0na2uJ9eMDxFxw11N3SfyCBlu7HJikaPD8g6Sg6FAV61jsqK2OO1XqFN4VTqoqJL~bbLm1Tx6bjQtbhwgmRoSHbsNmY8iya94ew7JI4Xw07XnW9w2vW5Q840vvCVlDxT1FDqGPSlN0bCseDyRXZLdY01KGFwrO10V52FBWjfT7DxB-x6lg0liaDhWOI60L9ANTNBEyLfKXWsyhw4wL9~IJ-zgyurnon7jHWc~3g7cZ3E0eEpx8jgQ__",
+    "https://s3-alpha-sig.figma.com/img/9f93/04b8/29f23c6b50f84f9c062369bfe28be75a?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JSyJ7Ek~dht0UuN8WGTlrwF9SO9ne5qyWK0XopJlXzOnO1hVS6U2Yzsauoh8PIp7HmmW4tWk3tWkwN~ONPZuXQ6u637aU-~1v9O-iYx5u4Tvw9OnsS0guDIYTjP6lyWv~ht-atzQbmnZr3bKVXG8ji98tVsmEtN2g69abxM7NpKMhKoxOiAlnAnr8XgVt4bBygNu45aMy9AlFQ9VldSiLA2hWZNeXephs4k4ZWt0KXxxSEVTCFSlhp84MLGW-6y~gfmUO3Tk5uXlOkqkYKmUvO~p2XcXf7mMMi6g7UPgxmzagVqqwxd76XA-JVKgt2z2~q1VSJv0PG8MY46NEtGY1A__"
+
+]
+var mySwiper = new Swiper ('.swiper', {
+
+    direction: 'vertical',
+    slidesPerView: 1,
+    loop: true,
+    
+    pagination: {
+      el: '.swiper-pagination',
+			clickable: true,
+            renderBullet: function (index, className) {
+                return (
+                  '<span class="' + className + '">' +
+                    '<img src="' + menu[index] + '" alt="Slide ' + (index + 1) +  '" style="width: 100%; height: 100%;" />' +
+                  '</span>'
+                );
+              },    
+      
+    },
+
+
+  })
