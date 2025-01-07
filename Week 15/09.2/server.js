d9f24475b3cb1e6fdb57e083148c98471274d71d
agasurfer <agasurfer@hotmail.com>
2025-01-07 11:59:39 +0100

    02 done

--- Week 15/09.2/server.js
+++ Week 15/09.2/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
+
+app.post("/", (req, res) => {
+    if(req.headers.authorization === "123") {
+        res.status(200).send("<h1>Hello World</h1>")
+    } else {
+        res.status(200).send("<h1>Not Authorized</h1>")
+    }
+})
+
 app.listen(port, () => {
     console.log(`Listening at ${port}`);
     
 })
+
